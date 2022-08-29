f6decbebb978162730c963dfa0904917b62d34b0
aravindreddymokireddy <aravindreddy.mokireddy@gmail.com>
2022-08-29 16:44:44 +0530

    validation

--- src/app/customer/cust-transaction/cust-transaction.component.ts
+++ src/app/customer/cust-transaction/cust-transaction.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'pp-cust-transaction',
@@ -8,9 +9,9 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 })
 export class CustTransactionComponent implements OnInit {
   custTransactionForm = this.formBuilder.group({
-    receiverAccountHolderNumber: "",
-    receiverBic: "",
-    currencyAmount: ""
+    receiverAccountHolderNumber: ['', Validators.required],
+    receiverBic:['', Validators.required],
+    currencyAmount: ['', Validators.required]
   });
 
   constructor(private formBuilder: FormBuilder) { }
@@ -21,6 +22,45 @@ export class CustTransactionComponent implements OnInit {
   onSubmit(): void {
     // Process checkout data here
     console.warn('Your transaction has been submitted', this.custTransactionForm.value);
+    this.sweetAlert();
     this.custTransactionForm.reset();
+  }
+
+
+  sweetAlert() {
+    const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: true
+  })
+
+  swalWithBootstrapButtons.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Yes, proceed!',
+    cancelButtonText: 'No, cancel!',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      swalWithBootstrapButtons.fire(
+        'Transaction Successful!',
+        'Your transaction has been recorded.',
+        'success'
+      )
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        'Cancelled',
+        'Your transaction has been cancelled :)',
+        'error'
+      )
+    }
+  })
   }
 }
--- src/app/employee/emp-transaction/emp-transaction.component.ts
+++ src/app/employee/emp-transaction/emp-transaction.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators, FormControl } from '@angular/forms';
 import Swal from 'sweetalert2';
 
 @Component({
@@ -10,23 +10,71 @@ import Swal from 'sweetalert2';
 export class EmpTransactionComponent implements OnInit {
 
   empTransactionForm = this.formBuilder.group({
-    customerId: "",
-    receiverAccountHolderNumber: "",
-    senderBic: "",
-    receiverBic: "",
-    currencyAmount: ""
+    customerId: ['', Validators.required],
+    receiverAccountHolderNumber: ['', Validators.required],
+    senderBic: ['', Validators.required],
+    receiverBic: ['', Validators.required],
+    currencyAmount: ['', Validators.required],
+
+  
   });
+
+  
+
   constructor(
     private formBuilder: FormBuilder,
   ) { }
 
   ngOnInit(): void {
+    
   }
 
   onSubmit() {
     console.warn('Your order has been submitted', this.empTransactionForm.value);
+    // if (this.empTransactionForm.value){
+      this.sweetAlert()
+    // }
     this.empTransactionForm.reset();
   }
+
+  sweetAlert() {
+    const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+      confirmButton: 'btn btn-success',
+      cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: true
+  })
+
+  swalWithBootstrapButtons.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Yes, proceed!',
+    cancelButtonText: 'No, cancel!',
+    reverseButtons: true
+  }).then((result) => {
+    if (result.isConfirmed) {
+      swalWithBootstrapButtons.fire(
+        'Transaction Successful!',
+        'Your transaction has been recorded.',
+        'success'
+      )
+    } else if (
+      /* Read more about handling dismissals below */
+      result.dismiss === Swal.DismissReason.cancel
+    ) {
+      swalWithBootstrapButtons.fire(
+        'Cancelled',
+        'Your transaction has been cancelled :)',
+        'error'
+      )
+    }
+  })
+  }
+
+
 
 
 }
